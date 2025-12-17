879c1841c7d706ca84a87abd93e7f3b9a1cab792
Lenovo <Lenovo@DESKTOP-PNAR7AQ>
2025-12-17 19:55:29 +0530

    Variables in java

---
+++ src/com/edubridge/MyClass.java
@@ -0,0 +1,20 @@
+package com.edubridge;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String name="Shanthala";
+       System.out.println(name);
+
+       int age=20;
+       System.out.println(age);
+
+       String course="Java";
+       System.out.println(course);
+
+       long fees=2000;
+       System.out.println(fees);
+	}
+
+}
